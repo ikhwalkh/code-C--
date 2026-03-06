a94caba61d30d8bf3172f1ab536ed3c58d35de9c
ikhwalkh <203380321+ikhwalkh@users.noreply.github.com>
2026-03-06 15:46:58 +0700

    void BubbleSort() {     cout << "\n===================" << endl;     cout << "Hasil Setiap Pass" << endl;     cout << "=================" << endl;     for (int pass = 1; pass < n; pass++)     {         for (int j = 0; j <= n - 1 - pass; j++)         {             if (a[j] > a[j + 1])             {                 int temp = a[j];                 a[j] = a[j + 1];                 a[j + 1] = temp;             }         }         cout << "pass ke -" << pass << ":\t";         for (int k = 0; k < n; k++)         {             cout << a[k] << "\t";         }         cout << endl;     } }

--- Praktikum3.cpp
+++ Praktikum3.cpp
@@ -54,4 +54,31 @@ void BubbleSort()
     cout << "\n===================" << endl;
     cout << "Hasil Setiap Pass" << endl;
     cout << "=================" << endl;
+    for (int pass = 1; pass < n; pass++)
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+        cout << "pass ke -" << pass << ":\t";
+        for (int k = 0; k < n; k++)
+        {
+            cout << a[k] << "\t";
+        }
+        cout << endl;
+    }
+}
+
+
+int main()
+{
+    input();
+    BubbleSort();
+    display();
+    return 0;
 }
